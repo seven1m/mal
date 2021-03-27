ff7b5778d7b94187d1dbdf3c615c6bb8f611b2b0
Tim Morgan <tim@timmorgan.org>
2021-03-27 16:50:05 -0500

    Step 0: add line editing and history with linenoise

--- impls/cpp/step0_repl.cpp
+++ impls/cpp/step0_repl.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#include "linenoise.hpp"
+
 std::string READ(std::string input) { return input; }
 
 std::string EVAL(std::string input) { return input; }
@@ -14,13 +16,19 @@ std::string rep(std::string input) {
 }
 
 int main() {
+  const auto history_path = "history.txt";
+  linenoise::LoadHistory(history_path);
+
   std::string input;
   for (;;) {
-    std::cout << "user> ";
-    if (!std::getline(std::cin, input))
+    auto quit = linenoise::Readline("user> ", input);
+    if (quit)
       break;
     std::cout << rep(input) << std::endl;
+    linenoise::AddHistory(input.c_str());
   }
+
+  linenoise::SaveHistory(history_path);
 
   return 0;
 }
