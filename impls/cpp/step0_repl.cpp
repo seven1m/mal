e70fb1ec9eb75785e5c677825e4f387a30a9d642
Tim Morgan <tim@timmorgan.org>
2021-03-27 20:46:57 -0500

    Fix clang-format config

--- impls/cpp/step0_repl.cpp
+++ impls/cpp/step0_repl.cpp
@@ -10,25 +10,25 @@ std::string EVAL(std::string input) { return input; }
 std::string PRINT(std::string input) { return input; }
 
 std::string rep(std::string input) {
-  auto ast = READ(input);
-  auto result = EVAL(ast);
-  return PRINT(result);
+    auto ast = READ(input);
+    auto result = EVAL(ast);
+    return PRINT(result);
 }
 
 int main() {
-  const auto history_path = "history.txt";
-  linenoise::LoadHistory(history_path);
+    const auto history_path = "history.txt";
+    linenoise::LoadHistory(history_path);
 
-  std::string input;
-  for (;;) {
-    auto quit = linenoise::Readline("user> ", input);
-    if (quit)
-      break;
-    std::cout << rep(input) << std::endl;
-    linenoise::AddHistory(input.c_str());
-  }
+    std::string input;
+    for (;;) {
+        auto quit = linenoise::Readline("user> ", input);
+        if (quit)
+            break;
+        std::cout << rep(input) << std::endl;
+        linenoise::AddHistory(input.c_str());
+    }
 
-  linenoise::SaveHistory(history_path);
+    linenoise::SaveHistory(history_path);
 
-  return 0;
+    return 0;
 }
